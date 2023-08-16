6faafd7bbf6c237e40797c59e0a02f3fd6de3255
Jessica C S Souza <101482545+cssjessica@users.noreply.github.com>
2023-08-16 14:22:17 -0500

    Update 9

--- LC9_PalindromeNumber.py
+++ LC9_PalindromeNumber.py
@@ -1,11 +1,33 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        c = str(x)
-        s = len(c)
-        for i in range(int(s/2)):
-            if c[i] != c[-1-i]:
-                return False
-        return True
-#
+        # converting to string
+        # c = str(x)
+        # s = len(c)
+        # for i in range(int(s/2)):
+        #     if c[i] != c[-1-i]:
+        #         return False
+        # return True
+
         # c = str(x)
         # return c == c[::-1]
+
+        # not converting to string
+
+        if x < 0:
+            return False
+            
+        div = 1
+        while x >= 10 * div:
+            div *= 10
+            
+        while x:
+            right = x % 10		# get right digit
+            left = x // div		# get left digit
+            
+            if left != right:
+                return False
+                
+            x = x % div	# remove left digit
+            x = x // 10	# remove right digit
+            div = div / 100
+        return True
