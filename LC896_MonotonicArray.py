31c6189808b884cf6c9173dbcce4fd2a38ffe5c5
Jessica C S Souza <101482545+cssjessica@users.noreply.github.com>
2023-09-29 14:48:44 -0500

    Update 896

--- LC896_MonotonicArray.py
+++ LC896_MonotonicArray.py
@@ -1,3 +1,12 @@
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
-        return nums == sorted(nums) or nums == sorted(nums, reverse=True)
+        # return nums == sorted(nums) or nums == sorted(nums, reverse=True)
+        increase, decrease = True, True
+
+        for i in range(len(nums) - 1):
+            if not (nums[i] <= nums[i + 1]):
+                increase = False
+            if not (nums[i] >= nums[i + 1]):
+                decrease = False
+            
+        return increase or decrease
