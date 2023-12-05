e45bb457af19d9e6675daa315699adac9b192fdc
Jessica C S Souza <101482545+cssjessica@users.noreply.github.com>
2023-12-05 08:32:27 -0600

    Add 1688

---
+++ LC1688_CountMatchesTournament.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        # time complexity: O(log2n)
+        # space complexity; O(1)
+        # res = 0
+
+        # while n > 1:
+        #     res += n // 2
+        #     n = math.ceil(n / 2)
+            
+        # return res
+
+        # time complexity: O(log2n)
+        # space complexity; O(1)
+        return n - 1
