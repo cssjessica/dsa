30caa65db97899e3d5f944e145663ee59bcc4257
Jessica C S Souza <101482545+cssjessica@users.noreply.github.com>
2023-12-23 11:10:36 -0800

    Add 261

---
+++ LC261_GraphValidTree.py
@@ -0,0 +1,39 @@
+from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param n: An integer
+    @param edges: a list of undirected edges
+    @return: true if it's a valid tree, or false
+    """
+    def valid_tree(self, n: int, edges: List[List[int]]) -> bool:
+    	# leetcode premium
+        # every node connected
+        # no loops
+
+        # time complexity: O(E+V)
+
+        if not n:
+            return True
+            
+        adj = { i: [] for i in range(n) }  # adjancency list
+        for n1, n2 in edges:
+            adj[n1].append(n2)
+            adj[n2].append(n1)
+            
+        visit = set()
+        def dfs(i, prev):
+            if i in visit:
+                return False
+                
+            visit.add(i)
+            for j in adj[i]:
+                if j == prev:
+                    continue
+                if not dfs(j, i):
+                    return False
+            return True
+            
+        return dfs(0, -1) and n == len(visit)
