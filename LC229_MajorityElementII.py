b6acd830f353f6e720523fb621ebc7f9d12fcaef
Jessica C S Souza <101482545+cssjessica@users.noreply.github.com>
2024-01-13 14:41:37 -0600

    Add 229

---
+++ LC229_MajorityElementII.py
@@ -0,0 +1,25 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        # time complexity: O(n)
+        # space complexity: O(1)
+
+        count = defaultdict(int)
+
+        for n in nums:
+            count[n] += 1
+            
+            if len(count) <= 2:
+                continue
+            
+            new_count = defaultdict(int)
+            for n, c in count.items():
+                if c > 1:
+                    new_count[n] = c - 1
+            count = new_count
+            
+        res = []
+        for n in count:
+            if nums.count(n) > len(nums) // 3:
+                res.append(n)
+
+        return res
