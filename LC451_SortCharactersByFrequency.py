7a5fb8e550b26bdce7c9359274a30a375b5c30b7
Jessica C S Souza <101482545+cssjessica@users.noreply.github.com>
2024-02-07 11:28:46 -0600

    Add 451

---
+++ LC451_SortCharactersByFrequency.py
@@ -0,0 +1,14 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        count = Counter(s) # char -> count
+        buckets = defaultdict(list) # freq -> [char]
+
+        for char, cnt in count.items():
+            buckets[cnt].append(char)
+
+        res = []
+        for i in range(len(s), 0, -1):
+            for c in buckets[i]:
+                res.append(c * i)
+
+        return "".join(res)
