fab9f3b79580756b24e14d3273a7ce72ca12369b
Jessica C S Souza <101482545+cssjessica@users.noreply.github.com>
2024-02-15 13:17:36 -0600

    Add 2971

---
+++ LC2971_FindPolygonWithLargestPerimeter.py
@@ -0,0 +1,10 @@
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort()
+        res = -1
+        total = 0
+        for n in nums:
+            if total > n:
+                res = total + n
+            total += n
+        return res
