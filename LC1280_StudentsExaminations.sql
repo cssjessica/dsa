2402e0ac84f18ac0931ab1775c7ffcbaaf5a88ca
Jessica C S Souza <101482545+cssjessica@users.noreply.github.com>
2024-07-17 18:11:21 -0500

    Add 1280

---
+++ LC1280_StudentsExaminations.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select stu.student_id, stu.student_name, sub.subject_name, count(exa.student_id) as attended_exams
+from Students stu
+cross join Subjects sub
+left join Examinations exa
+on stu.student_id = exa.student_id and sub.subject_name = exa.subject_name
+group by stu.student_id, stu.student_name, sub.subject_name
+order by stu.student_id, sub.subject_name
